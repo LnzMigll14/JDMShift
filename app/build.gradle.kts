018857240dfe12a1a2eb66aadbc3aa7842f7e3dd
LnzMigll14 <LanzMiguel14@gmail.com>
2024-12-05 17:00:45 +0800

    Files Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
+
 }
 
 android {
@@ -47,8 +48,12 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment:2.7.4")
     implementation("androidx.navigation:navigation-ui:2.7.4")
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
-    implementation("com.google.android.gms:play-services-location:18.0.0")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.firebase:firebase-auth:23.1.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.1.1")
+    implementation("com.google.firebase:firebase-storage:21.0.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -64,22 +69,14 @@ dependencies {
 
     implementation("androidx.recyclerview:recyclerview:1.2.1")
 
-    // Firebase
-    implementation("com.google.firebase:firebase-firestore:24.0.1")
-    implementation("com.google.firebase:firebase-storage:20.0.0")
 
 
     implementation("com.google.android.material:material:1.6.1")
-    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-auth:latest_version")
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
 
     implementation("com.google.android.exoplayer:exoplayer-core:2.19.1")
     implementation("com.google.android.exoplayer:exoplayer-ui:2.19.1")
 
 
     implementation("com.googlecode.libphonenumber:libphonenumber:8.12.36")
-
-    implementation("com.google.firebase:firebase-firestore:24.1.0")
-    implementation("com.google.firebase:firebase-auth:21.0.1")
-    implementation("com.google.gms:google-services:4.3.13") }
+}
